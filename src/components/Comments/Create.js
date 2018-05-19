4563c6ecd1d0482c92602b777e00f76e86102bf6
Pedro Madruga <pedro@occam.ooo>
2018-05-19 16:44:41 +0200

    accel init

--- src/components/Comments/Create.js
+++ src/components/Comments/Create.js
@@ -4,7 +4,29 @@ import { TextInput, Button } from 'react-native-paper';
 import { Accelerometer } from 'expo';
 
 export default class Create extends Component {
-  state = {};
+  state = {
+    hasPhoneRaised: false
+  };
+
+  componentDidMount() {
+    this.onRaisedPhone();
+  }
+
+  onRaisedPhone() {
+    Accelerometer.addListener(accelerometerData => {
+      if (accelerometerData.z < -1.4) {
+        this.setState({ hasPhoneRaised: true });
+        console.warn('accelerometer.y', accelerometerData.z);
+      } else {
+        this.setState({ hasPhoneRaised: false });
+      }
+    });
+  }
+
+  sendInfoToFirebase() {
+    if (this.state.hasPhoneRaised) console.warn('RAISED');
+    else console.warn('NOT RAISED');
+  }
 
   onPressPost = () => {
     this.setState({
