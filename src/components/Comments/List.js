2a0fac95eb53ed594e5a5898366153f5c145b986
Pedro Madruga <pedro@occam.ooo>
2018-05-19 14:48:57 +0200

    listview animations

--- src/components/Comments/List.js
+++ src/components/Comments/List.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, FlatList, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import React, { Component } from 'react';
+import { View, FlatList, StyleSheet, Text, TouchableHighlight, Animated } from 'react-native';
 import { ListItem } from 'react-native-paper';
 import { Colors, FontSizes } from '../../constants';
 import { RegularText } from '../StyledText';
@@ -17,21 +17,39 @@ export default ({ comments, onPressComment }) => (
   </View>
 );
 
-const Comment = ({ id, content, attendeeName, likes, onPressComment }) => (
-  <TouchableHighlight onPress={() => onPressComment({ id })}>
-    <View style={{ flex: 1, flexDirection: 'row', marginTop: 10 }}>
-      <View style={{ marginRight: 5, flexDirection: 'column', alignItems: 'center' }}>
-        <Ionicons name="md-thumbs-up" size={32} color="gray" />
-        <Text>{likes}</Text>
-      </View>
+class Comment extends Component {
+  state = {
+    fadeAnim: new Animated.Value(0)
+  };
+  componentDidMount() {
+    Animated.timing(this.state.fadeAnim, {
+      toValue: 1,
+      duration: 5000
+    }).start();
+  }
 
-      <View style={{ flexDirection: 'column' }}>
-        <Text style={{ padding: 5 }}>{content}</Text>
-        <Text style={{ color: Colors.faint }}>{attendeeName}</Text>
-      </View>
-    </View>
-  </TouchableHighlight>
-);
+  render() {
+    const { id, content, attendeeName, likes, onPressComment } = this.props;
+
+    return (
+      <Animated.View style={{ opacity: this.state.fadeAnim }}>
+        <TouchableHighlight onPress={() => onPressComment({ id })}>
+          <View style={{ flex: 1, flexDirection: 'row', marginTop: 10 }}>
+            <View style={{ marginRight: 5, flexDirection: 'column', alignItems: 'center' }}>
+              <Ionicons name="md-thumbs-up" size={32} color="gray" />
+              <Text>{likes}</Text>
+            </View>
+
+            <View style={{ flexDirection: 'column' }}>
+              <Text style={{ padding: 5 }}>{content}</Text>
+              <Text style={{ color: Colors.faint }}>{attendeeName}</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Animated.View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   attendeeName: {
